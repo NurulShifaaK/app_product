0f4f18532f1bcc6859d3446788a656868c725161
NurulShifaaK <nurulshifaak@gmail.com>
2026-02-16 10:39:55 +0530

    committed changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import product from './routes/productRoute.js';
+import cors from "cors";
 
 const app=express();
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: "*",  
+}));
 //Routes
 app.use("/api/v1/",product);
 
